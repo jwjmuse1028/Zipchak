80d2f4280f935df4e821f7c45cf9eaa8f2b7ab63
j-a-27 <jinana27@gmail.com>
2022-11-06 22:49:14 +0900

    진아 11/05 22:50 Root 및 RouterMain 관련 파일 생성

---
+++ src/frontend/src/RouteMain.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Menu from "./components/Menu";
+import {Route, Routes} from "react-router-dom";
+import Home from "./components/Home";
+import MemberList from "./member/MemberList";
+
+function RouteMain(props) {
+    return (
+        <div>
+            {/* 항상 나오게 할 컴포넌트는 Routes 밖에 넣는다 */}
+            <Menu/>
+            <br style={{clear:'both'}}/><br/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                {/*<Route path='/login' element={<LoginForm/>}/>*/}
+                <Route path='/member'>
+                    {/*<Route path='form' element={<MemberForm/>}/>*/}
+                    <Route path='list' element={<MemberList/>}/>
+                </Route>
+                {/*<Route path='/board'>*/}
+                {/*    <Route path='form' element={<BoardForm/>}/>*/}
+                {/*    <Route path='list' element={<BoardList/>}/>*/}
+                {/*    <Route path='list/:currentPage' element={<BoardList/>}/>*/}
+                {/*</Route>*/}
+                {/* 지정된 주소 외 주소는 잘못된 url주소라고 출력 */}
+                <Route path='*' element={
+                    <div>
+                        <h1>잘못된 URL 주소입니다</h1>
+                        {/*<br/>*/}
+                        {/*<img alt='' src={img1} width='400'/>*/}
+                    </div>
+                }/>
+            </Routes>
+        </div>
+    );
+}
+
+export default RouteMain;
