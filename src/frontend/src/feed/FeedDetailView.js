578f2aa93d2fe532421ab423a47fcbfe965f3939
j-a-27 <jinana27@gmail.com>
2022-11-17 18:12:24 +0900

    진아 11/17 18:12 feedDetail 관련 파일 수정33

--- src/frontend/src/feed/FeedDetailView.js
+++ src/frontend/src/feed/FeedDetailView.js
@@ -1,15 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import {useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
+import "../css/FeedList.css";
 
 function FeedDetailView(props) {
     const {fd_num}=useParams();
     const navi=useNavigate();
     const [fdata,setFdata]=useState('');
 
-    const detailUrl=localStorage.url+"/feed/detail?fd_num="+fd_num;
-    console.log("detailUrl:"+detailUrl);
     const getFeedDetail=()=>{
+        const detailUrl=localStorage.url+"/feed/detail?fd_num="+fd_num;
+        console.log("detailUrl:"+detailUrl);
+
         axios.get(detailUrl)
             .then(res=>{
                 console.log("success");
@@ -24,11 +26,16 @@ function FeedDetailView(props) {
 
     return (
         <div>
-            {/*<h5>{fdata.dto.fd_title}</h5>*/}
-            <img style={{width: '300px'}} src={`https://s3.ap-northeast-2.amazonaws.com/bitcampteam2/fd_img/${fdata.dto.fd_num}/${fdata.dto.fd_img}`}/>
+            { fdata &&
+            <div>
+            <h5>{fdata.dto.fd_title}</h5>
+            <img style={{width: '300px'}}
+                 src={`https://s3.ap-northeast-2.amazonaws.com/bitcampteam2/fd_img/${fd_num}/${fdata.dto.fd_img}`}/>
             {fdata.prf_map.prf_nick}
-            <img alt="" style={{width: '300px'}}
+            <img alt=""  className="project-feed__item__writer__image"
                  src={`https://s3.ap-northeast-2.amazonaws.com/bitcampteam2/prf_img/${fdata.prf_map.prf_img}`}/>
+            </div>
+            }
         </div>
     );
 }
