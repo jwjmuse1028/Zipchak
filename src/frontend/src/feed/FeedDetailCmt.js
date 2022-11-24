92bbce94ec1de97e8f494111f70a29c5fd45fe76
j-a-27 <jinana27@gmail.com>
2022-11-24 17:34:23 +0900

    진아 17/23 17:33 feed 댓글 페이징 관련 생성 feed 댓글 관련 파일 생성 및 수정33

--- src/frontend/src/feed/FeedDetailCmt.js
+++ src/frontend/src/feed/FeedDetailCmt.js
@@ -1,10 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import "../css/FeedCmt.css";
+import Paging from "./Paging";
 
 function FeedDetailCmt({fd_num}) {
 
     const [cmtlist, setCmtlist] = useState([]);
+    const [count, setCount] = React.useState(0); //아이템 총 개수
+    const [page, setPage] = React.useState(1); //현재페이지
+    const [postPerPage] = React.useState(7); //페이지당 아이템 개수
+
+    const [indexOfLastPost, setIndexOfLastPost] = React.useState(0);
+    const [indexOfFirstPost, setIndexOfFirstPost] = React.useState(0);
+    const [currentPosts, setCurrentPosts] = React.useState(0);
 
     const [cmt_num, setCmt_num] = useState(0);
     const [cmt_txt, setCmt_txt] = useState('');
@@ -14,6 +22,11 @@ function FeedDetailCmt({fd_num}) {
     const [reok, setReok] = useState(-1);
     const [cmtre_txt, setCmtre_txt] = useState('');
     const [cmt_btn_cls,setCmt_btn_cls]=useState("css-17ydrlm");
+    const [cmtre_btn_cls,setCmtre_btn_cls]=useState("css-17ydrlm");
+    const [cmt_fr_cls,setCmt_fr_cls]=useState("css-1hlhf3b");
+    const [cmtre_fr_cls,setCmtre_fr_cls]=useState("css-1hlhf3b");
+
+    const [oldpp,setOldpp]=useState(0);
 
     const ur_num = sessionStorage.ur_num;
     // 로그인한 사람 사진과 닉네임 출력(getPrfByNum으로 axios 가져오기 실패...)
@@ -28,16 +41,35 @@ function FeedDetailCmt({fd_num}) {
 
         axios.get(cmtUrl)
             .then(res => {
-                console.log("success");
                 setCmtlist(res.data);
+                setCount(res.data.length);
+                let pp=page * postPerPage;
+                let fp=postPerPage;
+                let cnt=0;
+                console.log(res.data[pp-1].cmt_rl);
+                while(res.data[pp-1].cmt_rl>0)
+                {
+                    console.log("whilepp:"+pp--);
+                    fp--;
+                    cnt++;
+                }
+                console.log(pp);
+                console.log(fp*page);
+                console.log(cnt);
+                // setIndexOfFirstPost(pp-fp);
+                setCurrentPosts(res.data.slice(pp-fp, pp-1));
+
             })
     }
     useEffect(() => {
         getAllCmt();
-    }, []);
+    }, [page, indexOfFirstPost, indexOfLastPost, postPerPage]);
+
 
     const onCmtSubmit = (e) => {
+
         e.preventDefault();
+
         setCmt_num(0);
         setCmt_rg(0);
         setCmt_rs(0);
@@ -53,13 +85,14 @@ function FeedDetailCmt({fd_num}) {
 
 
     }
-    const onCmtReSubmit =(cmt_num,cmt_rg,cmt_rs,cmt_rl)=> {
+    const onCmtReSubmit =(e,cmt_num,cmt_rg,cmt_rs,cmt_rl)=> {
+
+        e.preventDefault();
 
         setCmt_num(cmt_num);
         setCmt_rg(cmt_rg);
         setCmt_rs(cmt_rs);
         setCmt_rl(cmt_rl);
-
 
         axios.post(cmtinsertUrl, {cmt_num, fd_num, ur_num, "cmt_txt":cmtre_txt, cmt_rg, cmt_rs, cmt_rl})
             .then(res => {
@@ -67,37 +100,53 @@ function FeedDetailCmt({fd_num}) {
                 getAllCmt();
                 //초기화
                 setCmtre_txt('');
+                setCmt_num(0);
                 setCmt_rg(0);
                 setCmt_rs(0);
                 setCmt_rl(0);
+                setReok(-1);
                 }
             )
+    }
 
-
-
+    const onFocus=(e)=>{
+        setCmt_fr_cls("css-vg56");
+        setCmt_btn_cls("css-fw3oix");
+    }
+    const onBlur=(e)=>{
+        setCmt_fr_cls("css-1hlhf3b");
+        setCmt_btn_cls("css-17ydrlm");
+    }
+    const onRefocus=(e)=>{
+        setCmtre_fr_cls("css-vg56");
+        setCmtre_btn_cls("css-fw3oix");
+    }
+    const onReblur=(e)=>{
+        setCmtre_fr_cls("css-1hlhf3b");
+        setCmtre_btn_cls("css-17ydrlm");
     }
 
     return (
         <div className={"cmt_container"}>
             {ur_num &&
-                <form className="css-blglsf e1lhpdsz3"
-                      onClick={(e)=>setCmt_btn_cls("css-fw3oix")}>
+                <form className="css-blglsf e1lhpdsz3" onSubmit={onCmtSubmit}>
                     <img src={`${imgUrl}/prf_img/${prf_img}`}
-                         style={{height: '40px', width: '40px', borderRadius: '100%'}}/>
+                         class="css-1iaw8mx e1lhpdsz1"/>
                     <div className="e1lhpdsz0 css-juhhnt eaz7a9d5">
-                        <div className="comment-feed__form__content__text eaz7a9d4 css-1hlhf3b">
+                        <div className={`comment-feed__form__content__text eaz7a9d4 ${cmt_fr_cls}`}>
                             <div className="css-1ua7n09">
                                 <input placeholder="칭찬과 격려의 댓글은 작성자에게 큰 힘이 됩니다 :)"
                                        className="css-11bu34y" size="44" value={cmt_txt}
+                                       onFocus={onFocus}
+                                       onBlur={onBlur}
                                        onChange={(e) => {
                                            setCmt_txt(e.target.value);
-                                           setCmt_btn_cls("css-fw3oix");
                                        }}/>
                                 {/*<a className="mention"*/}
                                 {/*   href="/users/3879597">@영구르으</a>&nbsp;*/}
 
                                 <div className="css-1n3lsph">
-                                    <button type={"button"} className={cmt_btn_cls}
+                                    <button type={"submit"} className={cmt_btn_cls}
                                             onClick={onCmtSubmit}
                                             disabled="">입력</button>
                                 </div>
@@ -106,55 +155,75 @@ function FeedDetailCmt({fd_num}) {
                     </div>
                 </form>
             }
-            {cmtlist &&
-                cmtlist.map((cdto, idx) => (
+            {currentPosts &&
+                currentPosts.map((cdto, idx) => (
                     <div>
                         {/* 댓글 출력 부분 */}
                         {cdto.cmt_rl>0?
-                            <div style={{marginLeft: "20px"}}>
-                                <div className={"input-group"}>
-                                    <img src={`${imgUrl}/prf_img/${cdto.prf_img}`}
-                                         style={{height: '30px', width: '30px', borderRadius: '100%'}}/>
-                                    <span>{cdto.prf_nick}</span>
-                                </div>
-                                <div>{cdto.cmt_txt}</div>
+                            <div style={{marginLeft: "20px"}} class="css-f9yozn ewncmdz14">
+                                <li className="css-1fzkxhd ec4queb0">
+                                    <div className="css-ame6mi ewncmdz14">
+                                        <div className="css-eayw7z ewncmdz13"><a href="/users/11402519">
+                                            <img className="css-50jqym ewncmdz12" src={`${imgUrl}/prf_img/${cdto.prf_img}`}/>
+                                            <span className="css-nltycz ewncmdz11">{cdto.prf_nick}</span></a></div>
+                                        <div className={"input-group"}>
+                                            <p className="ewncmdz7 css-ofv0mw eyi4x8z0">{cdto.cmt_txt}</p>
+                                            <div className={"css-lw9a6t"}>
+                                                <button type={'button'} onClick={(e) =>{
+                                                    setReok(idx);
+                                                    setCmtre_txt("@"+cdto.prf_nick+"  ");
+                                                }} className={"css-lw9a6t"} style={{marginLeft:'10px'}}>답글달기</button>
+                                                &nbsp;·&nbsp;{cdto.cmt_wdate}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
                             </div>
                             :
                             <div>
-                                <div className={"input-group"}>
-                                    <img src={`${imgUrl}/prf_img/${cdto.prf_img}`}
-                                         style={{height: '30px', width: '30px', borderRadius: '100%'}}/>
-                                    <span>{cdto.prf_nick}</span>
-                                </div>
-                                <div>{cdto.cmt_txt}</div>
+                                <li className="css-1fzkxhd ec4queb0">
+                                    <div className="css-ame6mi ewncmdz14">
+                                        <div className="css-eayw7z ewncmdz13"><a href="/users/11402519">
+                                            <img className="css-50jqym ewncmdz12" src={`${imgUrl}/prf_img/${cdto.prf_img}`}/>
+                                            <span className="css-nltycz ewncmdz11">{cdto.prf_nick}</span></a></div>
+                                        <div className={"input-group"}>
+                                            <p className="ewncmdz7 css-ofv0mw eyi4x8z0">{cdto.cmt_txt}</p>
+                                            <div className={"css-lw9a6t"}>
+                                                <button type={'button'} onClick={(e) =>{
+                                                    setReok(idx);
+                                                    setCmtre_txt("@"+cdto.prf_nick+"  ");
+                                                }} className={"css-lw9a6t"} style={{marginLeft:'10px'}}>답글달기</button>
+                                                &nbsp;·&nbsp;{cdto.cmt_wdate}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
                             </div>
                         }
+
                         {ur_num &&
                             <div>
                                 {/* 대댓글 입력 부분 */}
-                                <button type={'button'} onClick={(e) =>{
-                                    setReok(idx);
-                                    setCmtre_txt("@"+cdto.prf_nick+"  ");
-                                }} className={"css-17ydrlm"}>답글달기</button>
                                 {reok == idx &&
                                         <form className="css-blglsf e1lhpdsz3" style={{marginLeft:'20px'}}
-                                              onClick={(e)=>setCmt_btn_cls("css-fw3oix")}>
+                                              onSubmit={(e)=>onCmtReSubmit(e,cdto.cmt_num,cdto.cmt_rg,cdto.cmt_rs,cdto.cmt_rl)}>
                                             <img src={`${imgUrl}/prf_img/${prf_img}`}
-                                                 style={{height: '40px', width: '40px', borderRadius: '100%'}}/>
+                                                 class="css-1iaw8mx e1lhpdsz1"/>
                                             <div className="e1lhpdsz0 css-juhhnt eaz7a9d5">
-                                                <div className="comment-feed__form__content__text eaz7a9d4 css-1hlhf3b">
+                                                <div className={`comment-feed__form__content__text eaz7a9d4 ${cmtre_fr_cls}`}>
                                                     <div className="css-1ua7n09">
                                                         <input placeholder="칭찬과 격려의 댓글은 작성자에게 큰 힘이 됩니다 :)"
-                                                             className="css-11bu34y" size="44" value={cmtre_txt}
+                                                               className="css-11bu34y" size="44" value={cmtre_txt}
+                                                               onFocus={onRefocus}
+                                                               onBlur={onReblur}
                                                                onChange={(e) => {
                                                                    setCmtre_txt(e.target.value);
-                                                                   setCmt_btn_cls("css-fw3oix");
                                                                }}/>
                                                             {/*<a className="mention"*/}
                                                             {/*   href="/users/3879597">@영구르으</a>&nbsp;*/}
 
                                                         <div className="css-1n3lsph">
-                                                            <button type={"button"} className={cmt_btn_cls}
+                                                            <button type={"submit"} className={cmtre_btn_cls}
                                                                     onClick={()=>{onCmtReSubmit(cdto.cmt_num,cdto.cmt_rg,cdto.cmt_rs,cdto.cmt_rl)}}
                                                                     disabled="">입력</button>
                                                         </div>
@@ -167,7 +236,7 @@ function FeedDetailCmt({fd_num}) {
                         }
                     </div>))
             }
-
+            <Paging count={count} page={page} setPage={setPage}/>
         </div>
     )
 }
