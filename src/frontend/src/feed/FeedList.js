aa673194e468f4b04d049db6fd98d2bcdbda2c58
j-a-27 <jinana27@gmail.com>
2022-12-01 10:46:31 +0900

    진아 12/01 10:45 feed list 검색, 정렬, 옵션선택 완료

--- src/frontend/src/feed/FeedList.js
+++ src/frontend/src/feed/FeedList.js
@@ -3,13 +3,14 @@ import "../css/FeedList.css";
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import RefreshIcon from '@mui/icons-material/Refresh';
 
 function FeedList(props) {
     const navi = useNavigate();
     const [feedlist, setFeedlist] = useState([]);
-    const [search_col,setSearch_col] = useState(null);
-    const [search_word,setSearch_word] = useState(null);
-    const [order_col,setOrder_col] = useState(null);
+    const [search_col,setSearch_col] = useState("");
+    const [search_word,setSearch_word] = useState("");
+    const [order_col,setOrder_col] = useState("");
     const [option, setOption] = useState({
         spc: '',
         lvtp: '',
@@ -43,6 +44,10 @@ function FeedList(props) {
         feedList();
     }, []);
 
+    useEffect(() => {
+        searchFeed();
+    }, [order_col]);
+
     const updaterdcnt=(fd_num)=>{
 
         const rdcntUrl=localStorage.url+"/feed/uprdcnt?fd_num="+fd_num;
@@ -54,11 +59,13 @@ function FeedList(props) {
     }
     const searchFeed = ()=>{
         console.log("search_col:"+search_col);
+        console.log("order_col:"+order_col);
         const searchUrl = localStorage.url + "/feed/list?search_col="+search_col+"&search_word="+search_word+"&order_col="+order_col;
-
+        console.log("searchUrl:"+searchUrl);
         axios.get(searchUrl)
             .then(res => {
                 setFeedlist(res.data);
+                console.log(res.data);
             })
     }
     const searchEnter = (e) => {
@@ -74,30 +81,30 @@ function FeedList(props) {
             [name]: value,
         })
     }
-    const getSearchList = () => {
-        // let result = feedlist;
-        // if(주거형태 != null) {
-        //     result.filter
-        // }
-        // ...
-        //
-        // return result;
-
-    // .option.lvtp==''?false:filter((data)=>data.fd_lvtp.includes(option.lvtp))
+    const refreshOps=()=>{
+        setOption({
+            spc: '',
+            lvtp: '',
+            fml: '',
+            style: '',
+        })
+        setOrder_col('');
+        setSearch_col('');
+        setSearch_word('');
     }
 
     return (
         <div className="feed_container">
             <div className="input-group" style={{margin:"20px auto"}}>
                 <select className="form-select fsel" name="order_col" style={{width: "10%",margin:"0 20px 0 0",maxWidth:'150px'}}
-                        onChange={(e)=>setOrder_col(e.target.value)}>
+                        onChange={(e)=>setOrder_col(e.target.value)} value={order_col}>
                     <option value="" selected disabled>정렬</option>
                     <option value="fd_num" >최신순</option>
                     <option value="fd_rdcnt">조회순</option>
                     <option value="fd_likes">좋아요순</option>
                 </select>
-                <select className="form-select fsel" name="style" style={{width: "10%",margin:"0 20% 0 0",maxWidth:'150px'}}
-                        onChange={setOptionSelect}>
+                <select className="form-select fsel" name="style" style={{width: "10%",margin:"0 30px 0 0",maxWidth:'150px'}}
+                        onChange={setOptionSelect} value={option.style}>
                     <option value="" selected disabled>스타일</option>
                     <option value="모던">모던</option>
                     <option value="미니멀&심플">미니멀&심플</option>
@@ -108,27 +115,19 @@ function FeedList(props) {
                     <option value="러블리&로맨틱">러블리&로맨틱</option>
                     <option value="한국&아시아">한국&아시아</option>
                 </select>
-                {/*<select className="form-select fsel" name="search_col"*/}
-                {/*        onChange={(e)=>setSearch_col(e.target.value)}>*/}
-                {/*    <option value="fd_title" >제목</option>*/}
-                {/*    <option value="prf_nick">작성자</option>*/}
-                {/*    <option value="fd_txt">내용</option>*/}
-                {/*</select>*/}
-                {/*<select className="form-select fsel" name="search_col"*/}
-                {/*        onChange={(e)=>setSearch_col(e.target.value)}>*/}
-                {/*    <option value="fd_title" >제목</option>*/}
-                {/*    <option value="prf_nick">작성자</option>*/}
-                {/*    <option value="fd_txt">내용</option>*/}
-                {/*</select>*/}
+                <button type={"button"} className="fbtn" onClick={refreshOps}>
+                    <RefreshIcon style={{fontSize:'16px',paddingBottom:'2px'}}/>초기화
+                </button>
                 <div className="input-group" style={{width: "40%",margin:"0 0 0 auto"}}>
                     <select className="form-select fsearch" style={{width: "100px"}} name="search_col"
-                        onChange={(e)=>setSearch_col(e.target.value)}>
+                        onChange={(e)=>setSearch_col(e.target.value)} value={search_col}>
+                        <option value="" selected disabled>선택</option>
                         <option value="fd_title">제목</option>
                         <option value="prf_nick">작성자</option>
                         <option value="fd_txt">내용</option>
                     </select>
                     &nbsp;&nbsp;&nbsp;
-                    <input type="text" name="search_word" className="form-control fsearch" style={{width: "150px"}}
+                    <input type="text" name="search_word" className="form-control fsearch" style={{width: "150px"}} value={search_word}
                         placeholder="검색단어" onChange={(e)=>setSearch_word(e.target.value)} onKeyPress={searchEnter}/>
                     <button type="button" className="btn btn-secondary"
                             style={{width: "50px",backgroundColor:"#35C5F0",border:"#35C5F0"}}
@@ -136,7 +135,7 @@ function FeedList(props) {
                 </div>
             </div>
 
-            <div style={{color:"#777", marginBottom:"10px"}}>총 {feedlist.length}개 게시물</div>
+            <div style={{color:"#777", marginBottom:"10px"}}>총 {feedlist.filter((data)=>data.fd_style.includes(option.style)).length}개 게시물</div>
 
             {/*<input type={'file'} id="fileimg" multiple required*/}
             {/*       style={{visibility: 'hidden'}} onChange={uploadDB}/>*/}
